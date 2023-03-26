35be2cbcbfc5505eefa4314df11a2cd1d2a4eca5
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-26 16:51:56 +0300

    :love: done with repository

--- app/src/main/java/com/steve_md/nftapp/network/NftApiService.kt
+++ app/src/main/java/com/steve_md/nftapp/network/NftApiService.kt
@@ -4,8 +4,14 @@
 
 package com.steve_md.nftapp.network
 
+import com.steve_md.nftapp.data.NftData
+import com.steve_md.nftapp.utils.Constants.TOP_NFT
 import retrofit2.http.GET
 
 interface NftApiService{
-    //@GET()
+    @GET(TOP_NFT)
+    suspend fun getTopNft() : List<NftData.Top>
+
+    @GET()
+    suspend fun getTrendingNft(): List<NftData.Trending>
 }
--- app/src/main/java/com/steve_md/nftapp/network/SafeApiCall.kt
+++ app/src/main/java/com/steve_md/nftapp/network/SafeApiCall.kt
@@ -4,3 +4,28 @@
 
 package com.steve_md.nftapp.network
 
+import retrofit2.HttpException
+
+
+interface SafeApiCall {
+
+    suspend fun <T> safeApiCall(
+        apiCall: suspend () -> T
+    ) : Resource<T> {
+        return try {
+            Resource.Success(apiCall.invoke())
+        } catch (throwable: Throwable) {
+            when(throwable) {
+                is HttpException -> {
+                    Resource.Failure(false, throwable.code(), throwable.response()?.errorBody())
+                }
+                else -> {
+                    Resource.Failure(true, null, null)
+                }
+            }
+        }
+    }
+
+
+
+}
