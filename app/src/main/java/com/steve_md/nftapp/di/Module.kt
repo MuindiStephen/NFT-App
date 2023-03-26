65a94f4d81f2b5de3965ab8560ba7e7d3687f036
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-26 17:06:25 +0300

    :rocket: adds hilt :smile:

--- app/src/main/java/com/steve_md/nftapp/di/Module.kt
+++ app/src/main/java/com/steve_md/nftapp/di/Module.kt
@@ -1,2 +1,46 @@
-package com.steve_md.nftapp.di
+/*
+ * Copyright (c) Stephen Muindi @2023
+ */
 
+package com.steve_md.nftapp.di
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.steve_md.nftapp.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Module  {
+
+    @Provides
+    fun providesNftBaseUrl() = BASE_URL
+
+    @Provides
+    fun providesGson() : Gson = GsonBuilder().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(gson: Gson) : Retrofit {
+        return Retrofit.Builder()
+            .client(
+                OkHttpClient.Builder().also {
+                    val httpLoggingInterceptor = HttpLoggingInterceptor()
+                    httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+                    it.addInterceptor(httpLoggingInterceptor)
+                }.build()
+            )
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+
+}
