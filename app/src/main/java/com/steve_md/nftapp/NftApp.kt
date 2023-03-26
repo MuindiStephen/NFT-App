e78b4b548e554f6290a54e4ba3d40035d7ad5138
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-26 17:14:34 +0300

    :rocket: adds hilt :smile: and configured baseapp at manifest to take hilt

--- app/src/main/java/com/steve_md/nftapp/NftApp.kt
+++ app/src/main/java/com/steve_md/nftapp/NftApp.kt
@@ -1,4 +1,12 @@
+/*
+ * Copyright (c) Stephen Muindi @2023
+ */
+
 package com.steve_md.nftapp
 
-class NftApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class NftApp : Application()
