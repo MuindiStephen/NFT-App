2e54c27438701d2bfa56275f893bef1325e4664b
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-27 15:51:58 +0300

    done with splash screen

--- app/src/main/java/com/steve_md/nftapp/network/SafeApiCall.kt
+++ app/src/main/java/com/steve_md/nftapp/network/SafeApiCall.kt
@@ -25,7 +25,4 @@ interface SafeApiCall {
             }
         }
     }
-
-
-
 }
--- app/src/main/java/com/steve_md/nftapp/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/steve_md/nftapp/ui/fragments/splash/SplashFragment.kt
@@ -5,13 +5,12 @@
 package com.steve_md.nftapp.ui.fragments.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.steve_md.nftapp.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.steve_md.nftapp.databinding.FragmentSplashBinding
-import com.steve_md.nftapp.ui.viewmodel.NFTViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -31,17 +30,19 @@ class SplashFragment : Fragment() {
             container,
             false
         )
-        return binding.root
+         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setUpBinding()
-    }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            hideActionBar()
+        }
+        private fun hideActionBar() {
+                (activity as AppCompatActivity).actionBar?.hide()
+        }
 
-    private fun setUpBinding() {
+   }
 
-    }
 
 
 
